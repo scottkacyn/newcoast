e6486ec9c28e99a187d54cf55b6afea26a593bfd
Scott Lieberman <scottklieberman@gmail.com>
2014-02-18 15:30:06 -0600

    Added Landings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Newcoast::Application.routes.draw do
+  get "landings/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
