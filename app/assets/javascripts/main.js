f4f92049d42cabaca753bc54c2033154d19b9f1c
Scott Kacyn <scott.kacyn@gmail.com>
2014-06-03 01:55:29 -0500

    Added custom pictures and text, cleaned up some interactions

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,36 +1,15 @@
 /*----------------------------------------------------*/
-// Flexslider in Top Banner
-/*----------------------------------------------------*/  
- $('#home-slider').flexslider({						
-		animation: "swing",
-		direction: "vertical",
-		slideshow: true,
-		slideshowSpeed: 3800,
-		animationDuration: 1000,
-		directionNav: false,
-		controlNav: true,
-		smootheHeight:true,
-		useCSS: false,
-		after: function(slider) {
-		  slider.removeClass('loading');
-		}
-			
- });
-
-/*----------------------------------------------------*/
 // Portfolio Project Loading
 /*----------------------------------------------------*/  
 
+$("#project-1").click(function(){
+	$('#result').load('landings/project-1.html.erb');
+}); 
 
-		   $("#project-1").click(function(){
-		   	$('#result').load('../portfolio/project-1.html');
-		   }); 
- 
-		   $("#page2").click(function(){
-		   	$('#result').load('pages/page2.html');
-		     //alert("Thanks for visiting!");
-		   });
-
+$("#page2").click(function(){
+	$('#result').load('pages/page2.html');
+ //alert("Thanks for visiting!");
+});
 
 /*----------------------------------------------------*/
 // Parallax Scrolling
@@ -40,5 +19,4 @@
 	$('.parallax .parallax-banner-1').parallax("50%", 0.6);
 	$('.parallax .parallax-banner-2').parallax("50%", 0.6);
 	$('.parallax .parallax-banner-3').parallax("50%", 0.6);
-
 })
